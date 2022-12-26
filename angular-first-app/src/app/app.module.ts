1e0a4110602621c757cf033f5ebc6b9655562ab3
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-26 14:23:44 +0100

    generate a component by command

--- angular-first-app/src/app/app.module.ts
+++ angular-first-app/src/app/app.module.ts
@@ -3,12 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import {ServerComponent} from "./server/server.component";
+import { ServersComponent } from './servers/servers.component';
 //decorator
 @NgModule({
   //registering components
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    ServersComponent
   ],
   imports: [
     BrowserModule

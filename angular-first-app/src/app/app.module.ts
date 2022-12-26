4500dc2360bed70ce1b8ed29c8f2068238c5303d
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-26 14:20:26 +0100

    using custom component

--- angular-first-app/src/app/app.module.ts
+++ angular-first-app/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import {ServerComponent} from "./server/server.component";
+//decorator
 @NgModule({
+  //registering components
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
     BrowserModule

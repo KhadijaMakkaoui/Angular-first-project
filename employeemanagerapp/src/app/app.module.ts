2190c102d265e284d80905d559b63d68c408244d
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-29 10:02:44 +0100

    display data from backend

--- employeemanagerapp/src/app/app.module.ts
+++ employeemanagerapp/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {EmployeeService} from "./employee.service";
 import {HttpClient, HttpClientModule} from "@angular/common/http";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import {HttpClient, HttpClientModule} from "@angular/common/http";
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule
   ],
   providers: [EmployeeService],
   bootstrap: [AppComponent]

7b7adc8dc92b4ab4eeb468165f0c970fda29a495
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2022-12-29 12:00:18 +0100

    create update,add,delete modals and create a methode to open the proper modal based on the mode(delete,update,add)

--- employeemanagerapp/src/app/app.component.ts
+++ employeemanagerapp/src/app/app.component.ts
@@ -28,4 +28,31 @@ export class AppComponent implements OnInit{
       }
     );
   }
+
+  /**
+   * This method will open the proper modal
+   * @param employee - the employee to be added
+   * @param mode - the mode of the modal(Add, Edit, Delete)
+   */
+  public onOpenModal(employee: Employee, mode: string): void {
+    const container = document.getElementById('main-container');
+    const button = document.createElement('button');
+    button.type = 'button';
+    button.style.display = 'none';
+    button.setAttribute('data-toggle', 'modal');
+    //check the mode and set the proper modal
+    if (mode=='add'){
+      button.setAttribute('data-target', '#addEmployeeModal');
+    }
+    if (mode=='edit'){
+      button.setAttribute('data-target', '#updateEmployeeModal');
+    }
+    if (mode=='delete'){
+      button.setAttribute('data-target', '#deleteEmployeeModal');
+    }
+    //append the button to the container
+    // @ts-ignore
+    container.appendChild(button);
+    button.click();
+  }
 }

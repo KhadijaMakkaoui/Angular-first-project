770abdd675cbdfa383e5e6d912960ac2c3ec79d1
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-01-05 11:52:22 +0100

    create a dummy recepie

--- employeemanagerapp/src/app/recepies/recepie-list/recepie-list.component.ts
+++ employeemanagerapp/src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {Recepie} from "../recepie.model";
 
 @Component({
   selector: 'app-recepie-list',
@@ -6,7 +7,11 @@ import {Component, OnInit} from '@angular/core';
   styleUrls: ['./recepie-list.component.css']
 })
 export class RecepieListComponent implements OnInit{
-  recepies=[];
+  recepies:Recepie[]=[
+    new Recepie('A Test Recepie',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    )];
   constructor() {
   }
   ngOnInit(): void {

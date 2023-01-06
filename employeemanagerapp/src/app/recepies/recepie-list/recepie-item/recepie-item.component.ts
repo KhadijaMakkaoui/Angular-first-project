c1041c7a33f64782e3ae1534ade44789d0834ff0
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-01-06 15:20:28 +0100

    Passing Recipe Data with Property Binding

--- employeemanagerapp/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
+++ employeemanagerapp/src/app/recepies/recepie-list/recepie-item/recepie-item.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Recepie} from "../../recepie.model";
 
 @Component({
   selector: 'app-recepie-item',
   templateUrl: './recepie-item.component.html',
   styleUrls: ['./recepie-item.component.css']
 })
-export class RecepieItemComponent {
+export class RecepieItemComponent implements OnInit {
+  @Input() recepie: Recepie = new Recepie('A Test Recepie', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg');
+  constructor() { }
+  ngOnInit() {
+  }
 
 }
+

a44126f9442152fc0683cbed0ed65253eaf9dffd
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2023-01-15 11:42:44 +0100

    Managing Recipes in a Recipe Service

--- employeemanagerapp/src/app/recepies/recepie-list/recepie-list.component.ts
+++ employeemanagerapp/src/app/recepies/recepie-list/recepie-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recepie} from "../recepie.model";
+import {RecepieService} from "../recepie.service";
 
 @Component({
   selector: 'app-recepie-list',
@@ -8,24 +9,12 @@ import {Recepie} from "../recepie.model";
 })
 export class RecepieListComponent implements OnInit{
   @Output() recipeWasSelected=new EventEmitter<Recepie>();
-  recepies:Recepie[]=[
-    new Recepie('A Test Recepie1',
-      'This is simply a test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
-    ),
-    new Recepie('A Test Recepie2',
-      'This is simply a test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
-    ),
-    new Recepie('A Test Recepie3',
-      'This is simply a test',
-      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
-    )
-  ];
+  recepies:Recepie[]=[];
 
-  constructor() {
+  constructor(private recepieService:RecepieService) {
   }
   ngOnInit(): void {
+    this.recepies=this.recepieService.getRecepies();
   }
 
   onRecipeSelected(recipe:Recepie){
--- employeemanagerapp/src/app/recepies/recepie.service.ts
+++ employeemanagerapp/src/app/recepies/recepie.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {Recepie} from "./recepie.model";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RecepieService {
-
+  private recepies:Recepie[]=[
+    new Recepie('A Test Recepie1',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
+    new Recepie('A Test Recepie2',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    ),
+    new Recepie('A Test Recepie3',
+      'This is simply a test',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'
+    )
+  ];
   constructor() { }
+  getRecepies(){
+    //get a copy of the array
+    return this.recepies.slice();
+  }
 }
